f98a2c33e4e6fb369e4981c80a0b4f5e4aa5f9a0
minhdang <minhlethanhdang.datayoru2936@gmail.com>
2025-03-15 19:48:17 +0700

    troll problem

--- Assignment_3/P3_01.cpp
+++ Assignment_3/P3_01.cpp
@@ -21,28 +21,17 @@ const int MAXN = 100000;
 
 void NhapMang(int a[], int &n)
 {
-	for (int i = 0; i < n; i++)
-		cin >> a[i];	
+		
 }
 
 void selection_sort_ascending(int a[], int n)
 {
-	for (int i = 0; i < n - 1; i++)
-	{
-		int min_idx = i;
-		for (int j = i + 1; j < n; j++)
-			if (a[j] < a[min_idx])
-				min_idx = j;
-		swap(a[i], a[min_idx]);
-	}
+	
 }
 
 bool is_ascending(int a[], int n)
 {
-	for (int i = 1; i < n; i++)
-		if (a[i] < a[i-1])
-			return false;
-	return true;
+	return true
 }
 
 int main() {
@@ -58,3 +47,4 @@ int main() {
 
     return 0;
 }
+//bị giáo viên troll :))
