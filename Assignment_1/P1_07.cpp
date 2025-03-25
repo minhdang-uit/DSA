33c58bacb927de5f77f631077605005144f640df
minhdang-uit <minhlethanhdang.datayoru2936@gmail.com>
2025-03-25 18:43:51 +0700

    cc

--- Assignment_1/P1_07.cpp
+++ Assignment_1/P1_07.cpp
@@ -14,14 +14,15 @@ int binary_search_greatestLesser_last_occurrence(int a[], int n, int x)
     int left = 0, right = n - 1;
     while (left <= right)
     {
-        int mid = (left + right) /2;
-        if (a[mid] >= x) 
+        int mid = (left + right) / 2;
+        if (a[mid] >= x)
         {
+            result = mid;
             right = mid - 1;
         }
         else
         {
-            result = mid;
+            
             left = mid + 1;
         }
     }
